df153942da56a6776e4ce0d07e7d9447ee98638d
Tomislav Busic <tomislav.busic89@gmail.com>
2023-01-12 20:31:10 +0100

    categories

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { CardCategory } from "../Components/CardCategory";
 
-export const Home = () => {
+export const Categories = () => {
   const categories = useSelector((state) => state.categories.categoriesList);
 
   return (
