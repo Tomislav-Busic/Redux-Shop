08fceebe7f6661ebff958d16dc03188ef0ac4ead
Tomislav Busic <tomislav.busic89@gmail.com>
2023-01-15 18:29:07 +0100

    style header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
+import "./Header.scss";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./Header.scss";
 import { Link } from "react-router-dom";
 import { HeaderControllers } from "./HeaderControllers";
 
@@ -9,7 +9,6 @@ export const Header = () => {
   return (
     <Navbar bg="dark" variant="dark" fixed="top">
       <Container className="d-flex flex-wrap">
-        <Navbar.Brand href="#home">Shop Redux</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link as={Link} to="/">
             Categories
