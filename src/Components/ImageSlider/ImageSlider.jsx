8f7e4d91c9d54d4e8c81fe9c44a3d66f220ace77
Tomislav Busic <tomislav.busic89@gmail.com>
2023-01-19 17:26:35 +0100

    image slider

--- src/Components/ImageSlider/ImageSlider.jsx
+++ src/Components/ImageSlider/ImageSlider.jsx
@@ -1,48 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./ImageSlider.scss";
 
 export const ImageSlider = ({ images }) => {
-  let imgNum = 0;
-
-  const hideImages = () => {
-    images.forEach((img) => (img.style.display = "none"));
-  };
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const moveRight = () => {
-    hideImages();
-
-    imgNum++;
-
-    if (imgNum === images.length) {
-      imgNum = 0;
-    }
-
-    images[imgNum].style.display = "block";
+    const isLastSlide = currentIndex === images.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
   };
 
   const moveLeft = () => {
-    hideImages();
-
-    imgNum--;
-
-    if (imgNum === -1) {
-      imgNum = images.length - 1;
-    }
-
-    images[imgNum].style.display = "block";
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? images.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
   };
 
   return (
     <div className="images-slider">
-      <Button onClick={moveLeft}>Prev</Button>
+      <svg
+        onClick={moveLeft}
+        className="btn"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 384 512"
+      >
+        <path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z" />
+      </svg>
       <div className="images-container">
-        {images?.map((img, index) => (
-          <img style={{display: }} src={img} key={index} />
-        ))}
+        <img src={images[currentIndex]} />
       </div>
-      <Button onClick={moveRight}>Next</Button>
+      <svg
+        onClick={moveRight}
+        className="btn"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 384 512"
+      >
+        <path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z" />
+      </svg>
     </div>
   );
 };
