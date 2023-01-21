d0b87875e4bd5776550236ad130ec821df913099
Tomislav Busic <tomislav.busic89@gmail.com>
2023-01-21 13:01:21 +0100

    footer design, date, link

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Footer.scss";
+import { useSelector } from "react-redux";
 
 export const Footer = () => {
+  const theme = useSelector((state) => state.theme.themeState);
+
+  let date = new Date();
+  let today = date.getFullYear();
+
   return (
-    <div className="footer">
-      <p>Footer</p>
+    <div className={`footer ${theme && "footer-theme"}`}>
+      <p>
+        Copyright &copy; {today}{" "}
+        <a href="http://xn--tomislavbui-vlb28i.com/">Created by TBW</a>
+      </p>
     </div>
   );
 };
